2968818302bb5a0ca333bf2cf25a4a87db1f8185
Linux <linux_1901ee33@iitp.ac.in>
2021-10-05 21:25:49 +0530

    Create Level_order_traversal_line_by_line.cpp

---
+++ Data Structures/Tree/Level_order_traversal_line_by_line.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void printLevel(Node *root){
+    if(root==NULL)return;
+    queue<Node *>q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size()>1){
+        Node *curr=q.front();
+        q.pop();
+        if(curr==NULL){
+            cout<<"\n";
+            q.push(NULL);
+            continue;
+        }
+        cout<<curr->key<<" ";
+        if(curr->left!=NULL)
+            q.push(curr->left);
+        if(curr->right!=NULL)
+            q.push(curr->right);
+    }
+}  
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+	
+	printLevel(root);
+}
