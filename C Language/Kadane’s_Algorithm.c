d0450a119106b6e5e1c25a2fce8913359b23a068
Rohan Raju Meshram <meshramrohan786@gmail.com>
2022-10-04 01:40:08 +0530

    Added Kadanes Algorithm.c

---
+++ C Language/Kadane’s_Algorithm.c
@@ -0,0 +1,39 @@
+/*
+
+   Name: Rohan Raju Meshram
+   Problem Statement: We are given with an array and we need to find the largest contiguous subarray sum,
+                      which can be done using Kadane’s algorithm efficiently. 
+
+*/
+
+#include <stdio.h>
+
+#include <limits.h>
+
+int main() {
+  int n;
+  scanf("%d", & n);
+
+  int arr[n];
+
+  for (int i = 0; i < n; i++)
+    scanf("%d", & arr[i]);
+
+  int max_sum = INT_MIN, curr_sum = 0;
+
+  for (int i = 0; i < n; i++) {
+
+    curr_sum += arr[i];
+
+    if (max_sum < curr_sum)
+      max_sum = curr_sum;
+
+    if (curr_sum < 0)
+      curr_sum = 0;
+
+  }
+
+  printf("%d ", max_sum);
+
+  return 0;
+}
