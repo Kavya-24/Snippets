0247bcd4dbdd0dab8b27d4b6ae94b2b0d1bab603
varshakqr <88935911+varshakqr@users.noreply.github.com>
2022-10-18 19:30:13 +0530

    Add files via upload

---
+++ Data Structures/Tree/Tree Traversals/postorderTraversal.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// A binary tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+// Creating a new tree node
+Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Printing the nodes in inorder
+void printInorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    // left child
+    printInorder(node->left);
+
+    // right child
+    printInorder(node->right);
+
+    // data of node
+    cout << node->data << " ";
+}
+
+int main()
+{
+    struct Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->left->right->right = newNode(6);
+    cout << "\nInorder traversal of binary tree is \n";
+    printInorder(root);
+    return 0;
+}
