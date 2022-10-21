8ac6e98eca687576ff7876a31b709d2a13357d98
KumarDevada <115659253+KumarDevada@users.noreply.github.com>
2022-10-21 20:09:33 +0530

    Create Modified_Kaprekar_Number.java
    In mathematics, a Kaprekar number for a given base is a non-negative integer, the representation of whose square in that base can be split into two parts that add up to the original number again. For instance, 45 is a Kaprekar number, because 45² = 2025 and 20+25 = 45. This code gives all kaprekar numbers within a given range.

---
+++ Java/Modified_Kaprekar_Number.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'kaprekarNumbers' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER q
+     */
+
+    public static void kaprekarNumbers(int p, int q) {
+    // Write your code here
+    int count=0;
+    for(int i=p;i<=q;i++) {
+        if(i == 1) {
+            System.out.print(1+" ");
+        }
+        else if(i < 4) {
+            continue;
+        }
+        else {
+            long j = (long)i*i;
+            Long ff = j;
+            String c = ff.toString();
+            String f = c.substring(0,(c.length()/2));
+            String s = c.substring((c.length()/2));
+            long k = Long.parseLong(f);
+            long l = Long.parseLong(s);
+            
+            if((k+l) == i) {
+                System.out.print(i+" ");
+                count++;
+            }
+            
+        }
+    }
+    if(count == 0) {
+        System.out.println("INVALID RANGE");
+    }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.kaprekarNumbers(p, q);
+
+        bufferedReader.close();
+    }
+}
