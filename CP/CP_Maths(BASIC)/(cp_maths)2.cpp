a0aeb9e7410b96af8f5b83861539811a77c6fcad
ANUKOOL324 <anukoolbhul324@gmail.com>
2024-10-03 19:31:54 +0530

    kk

---
+++ CP/CP_Maths(BASIC)/(cp_maths)2.cpp
@@ -0,0 +1,21 @@
+// GCD-GREATEST COMMON DIVISIOR
+
+// FOR LCM = A*B/GCD(A,B)
+#include <iostream>
+using namespace std;
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << "\n";
+    // cout<<"lcm :"<<a*b/(gcd(a,b))<<endl;
+    return 0;
+}
